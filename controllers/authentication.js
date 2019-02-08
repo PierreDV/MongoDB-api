15298ad1f3f46f2a05ce6afda05a4efdbf2ce0bc
PierreDV <p.verteuil@gmail.com>
2019-02-08 10:26:18 +0100

    pull email and password off of request body

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,7 @@
 exports.signup = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	
 	// See if a user with the given email exists
 
 	// If a user with email does exist, return an error
