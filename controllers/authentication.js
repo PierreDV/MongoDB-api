49f62333a97ef836f793296181a30eba2bd854f4
PierreDV <p.verteuil@gmail.com>
2019-02-08 10:19:34 +0100

    outline steps needed to create user signup controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,9 @@
 exports.signup = (req, res, next) => {
-  res.send({ success: 'true' });
+	// See if a user with the given email exists
+
+	// If a user with email does exist, return an error
+
+	// If a user with email does NOT exist, create and save user record
+
+	// Respond to request indicating the user was created
 };
