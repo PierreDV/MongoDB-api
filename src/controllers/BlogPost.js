516870e13835f44bc76d0416d74a3ca1afcefc63
PierreDV <p.verteuil@gmail.com>
2019-02-10 22:18:28 +0100

    convert tabs to spaces

--- src/controllers/BlogPost.js
+++ src/controllers/BlogPost.js
@@ -5,90 +5,90 @@ import db from '../db';
 const findOneQuery = 'SELECT * FROM blog_posts WHERE id = $1';
 
 const BlogPost = {
-	async create(req, res) {
-		const createQuery = `INSERT INTO
-			blog_posts(id, title, body_text, author_id, created_at, updated_at)
-			VALUES($1, $2, $3, $4, $5, $6)
-			returning *`;
-		const values = [
-			uuidv4(),
-			req.body.title,
-			req.body.body_text,
-			req.user.id,
-			moment(new Date()),
-			moment(new Date())
-		];
+  async create(req, res) {
+    const createQuery = `INSERT INTO
+      blog_posts(id, title, body_text, author_id, created_at, updated_at)
+      VALUES($1, $2, $3, $4, $5, $6)
+      returning *`;
+    const values = [
+      uuidv4(),
+      req.body.title,
+      req.body.body_text,
+      req.user.id,
+      moment(new Date()),
+      moment(new Date())
+    ];
 
-		try {
-			const { rows } = await db.query(createQuery, values);
-			return res.status(201).send(rows[0]);
-		} catch(error) {
-			return res.status(400).send(error);
-		}
-	},
-	async getAll(req, res) {
-		const findAllQuery = 'SELECT * FROM blog_posts';
-		try {
-			const { rows, rowCount } = await db.query(findAllQuery, []);
-			return res.status(200).send({ rows, rowCount });
-		} catch(error) {
-			return res.status(400).send(error);
-		}
-	},
-	async getAllFromAuthor(req, res) {
-		const findAllFromAuthorQuery = 'SELECT * FROM blog_posts WHERE author_id = $1';
-		try {
-			const { rows, rowCount } = await db.query(findAllFromAuthorQuery, [req.params.id]);
-			return res.status(200).send({ rows, rowCount });
-		} catch(error) {
-			return res.status(400).send(error);
-		}
-	},
-	async getOne(req, res) {
-		try {
-			const { rows } =await db.query(findOneQuery, [req.params.id]);
-			if(!rows[0]) {
-				return res.status(404).send({'message': 'blog post not found.'});
-			}
-			return res.status(200).send(rows[0]);
-		} catch(error) {
-			return res.status(400).send(error);
-		}
-	},
-	async update(req, res) {
-		const updateQuery = `UPDATE blog_posts
-			SET title=$1, body_text=$2, updated_at=$3
-			WHERE id=$4 AND author_id=$5 returning *`;
-		try {
-			const { rows } = await db.query(findOneQuery, [req.params.id, req.user.id]);
-			if(!rows[0]) {
-				return res.status(404).send({'message': 'blog post not found'});
-			}
-			const values = [
-				req.body.title || rows[0].title,
-				req.body.body_text || rows[0].body_text,
-				moment(new Date()),
-				req.params.id,
-				req.user.id
-			];
-			const response = await db.query(updateQuery, values);
-			return res.status(200).send(response.rows[0]);
-		} catch(error) {
-			return res.status(400).send(error);
-		}
-	},
-	async delete(req, res) {
-		const deleteQuery = 'DELETE FROM blog_posts WHERE id=$1 AND author_id = $2 returning *';
-		try {
-			const { rows } = await db.query(deleteQuery, [req.params.id, req.user.id]);
-			if(!rows[0]) {
-				return res.status(404).send({'message': 'blog post not found'});
-			}
-			return res.status(204).send({'message': 'blog post deleted'});
-		} catch(error) {
-			return res.status(400).send(error);
-		}
-	}
+    try {
+      const { rows } = await db.query(createQuery, values);
+      return res.status(201).send(rows[0]);
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  },
+  async getAll(req, res) {
+    const findAllQuery = 'SELECT * FROM blog_posts';
+    try {
+      const { rows, rowCount } = await db.query(findAllQuery, []);
+      return res.status(200).send({ rows, rowCount });
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  },
+  async getAllFromAuthor(req, res) {
+    const findAllFromAuthorQuery = 'SELECT * FROM blog_posts WHERE author_id = $1';
+    try {
+      const { rows, rowCount } = await db.query(findAllFromAuthorQuery, [req.params.id]);
+      return res.status(200).send({ rows, rowCount });
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  },
+  async getOne(req, res) {
+    try {
+      const { rows } =await db.query(findOneQuery, [req.params.id]);
+      if(!rows[0]) {
+        return res.status(404).send({'message': 'blog post not found.'});
+      }
+      return res.status(200).send(rows[0]);
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  },
+  async update(req, res) {
+    const updateQuery = `UPDATE blog_posts
+      SET title=$1, body_text=$2, updated_at=$3
+      WHERE id=$4 AND author_id=$5 returning *`;
+    try {
+      const { rows } = await db.query(findOneQuery, [req.params.id, req.user.id]);
+      if(!rows[0]) {
+        return res.status(404).send({'message': 'blog post not found'});
+      }
+      const values = [
+        req.body.title || rows[0].title,
+        req.body.body_text || rows[0].body_text,
+        moment(new Date()),
+        req.params.id,
+        req.user.id
+      ];
+      const response = await db.query(updateQuery, values);
+      return res.status(200).send(response.rows[0]);
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  },
+  async delete(req, res) {
+    const deleteQuery = 'DELETE FROM blog_posts WHERE id=$1 AND author_id = $2 returning *';
+    try {
+      const { rows } = await db.query(deleteQuery, [req.params.id, req.user.id]);
+      if(!rows[0]) {
+        return res.status(404).send({'message': 'blog post not found'});
+      }
+      return res.status(204).send({'message': 'blog post deleted'});
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  }
 }
 
 export default BlogPost;
--- src/controllers/User.js
+++ src/controllers/User.js
@@ -4,57 +4,57 @@ import db from '../db';
 import Helper from './Helper';
 
 const User = {
-	async create(req, res) {
-		if (!req.body.email || !req.body.password) {
-			return res.status(400).send({'message': 'Some values are missing.'});
-		}
-		if (!Helper.isValidEmail(req.body.email)) {
-			return res.status(400).send({'message': 'Please enter a valid email address.'});
-		}
-		const hashPassword = Helper.hashPassword(req.body.password);
+  async create(req, res) {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).send({'message': 'Some values are missing.'});
+    }
+    if (!Helper.isValidEmail(req.body.email)) {
+      return res.status(400).send({'message': 'Please enter a valid email address.'});
+    }
+    const hashPassword = Helper.hashPassword(req.body.password);
 
-		const createQuery = `INSERT INTO
-			users(id, email, password, created_at, updated_at)
-			VALUES($1, $2, $3, $4, $5)
-			returning *`;
-		const values = [
-			uuidv4(),
-			req.body.email,
-			hashPassword,
-			moment(new Date()),
-			moment(new Date())
-		];
+    const createQuery = `INSERT INTO
+      users(id, email, password, created_at, updated_at)
+      VALUES($1, $2, $3, $4, $5)
+      returning *`;
+    const values = [
+      uuidv4(),
+      req.body.email,
+      hashPassword,
+      moment(new Date()),
+      moment(new Date())
+    ];
 
-		try {
-			const { rows } = await db.query(createQuery, values);
-			const token = Helper.generateToken(rows[0].id);
-			return res.status(201).send({ token });
-		} catch(error) {
-			if(error.routine === '_bt_check_unique') {
-				return res.status(400).send({ 'message': 'User with that email already exists.'});
-			}
-			return res.status(400).send(error);
-		}
-	},
-	async login(req, res) {
-		if(!req.body.email || !req.body.password) {
-			return res.status(400).send({'message': 'Some values are missing.'});
-		}
-		if(!Helper.isValidEmail(req.body.email)) {
-			return res.status(400).send({'message': 'Please enter a valid email address.'});
-		}
-		const text = 'SELECT * FROM users WHERE email = $1';
-		try {
-			const { rows } = await db.query(text, [req.body.email]);
-			if(!rows[0]) {
-				return res.status(400).send({'message': 'The credentials provided are invalid.'});
-			}
-			const token = Helper.generateToken(rows[0].id);
-			return res.status(200).send({ token });
-		} catch(error) {
-			return res.status(400).send(error);
-		}
-	}
+    try {
+      const { rows } = await db.query(createQuery, values);
+      const token = Helper.generateToken(rows[0].id);
+      return res.status(201).send({ token });
+    } catch(error) {
+      if(error.routine === '_bt_check_unique') {
+        return res.status(400).send({ 'message': 'User with that email already exists.'});
+      }
+      return res.status(400).send(error);
+    }
+  },
+  async login(req, res) {
+    if(!req.body.email || !req.body.password) {
+      return res.status(400).send({'message': 'Some values are missing.'});
+    }
+    if(!Helper.isValidEmail(req.body.email)) {
+      return res.status(400).send({'message': 'Please enter a valid email address.'});
+    }
+    const text = 'SELECT * FROM users WHERE email = $1';
+    try {
+      const { rows } = await db.query(text, [req.body.email]);
+      if(!rows[0]) {
+        return res.status(400).send({'message': 'The credentials provided are invalid.'});
+      }
+      const token = Helper.generateToken(rows[0].id);
+      return res.status(200).send({ token });
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  }
 }
 
 export default User;
