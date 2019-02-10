add557b27f915e1d4eb02647024dd1fd5fe73d35
PierreDV <p.verteuil@gmail.com>
2019-02-10 15:04:55 +0100

    add command line scripts to populate db with user and blogpost tables as well as scripts to remove them

---
+++ db.js
@@ -0,0 +1,109 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
+Pool.once('connect', () => {
+	console.log('connected to the db');
+});
+
+const createUserTable = () => {
+	const queryText = 
+		`CREATE TABLE IF NOT EXISTS
+			users(
+				id UUID PRIMARY KEY,
+				email VARCHAR(128) UNIQUE NOT NULL,
+				password VARCHAR(128) NOT NULL,
+				created_at TIMESTAMP,
+				modified_at TIMESTAMP
+			)`;
+	
+	pool.query(queryText)
+		.then((res) => {
+			console.log(res);
+			pool.end();
+		})
+		.catch((err) => {
+			console.log(err);
+			pool.end();
+		});
+};
+
+const createBlogPostTable = () => {
+	const queryText =
+		`CREATE TABLE IF NOT EXISTS
+			blog_posts(
+				id UUID PRIMARY KEY,
+				title VARCHAR(128) NOT NULL,
+				body_text TEXT NOT NULL,
+				author_id UUID NOT NULL,
+				created_at TIMESTAMP,
+				modified_at TIMESTAMP,
+				FOREIGN KEY (author_id) REFERTENCES users (id) ON DELETE CASCADE
+			)`;
+	pool.query(queryText)
+		.then((res) => {
+			console.log(res);
+			pool.end();
+		})
+		.catch((err) => {
+			console.log(err);
+			pool.end();
+		});
+};
+
+const dropUserTable = () => {
+	const queryText = 'DROP TABLE IF EXISTS users returning *';
+	pool.query(queryText)
+		.then((res) => {
+			console.log(res);
+			pool.end();
+		})
+		.catch((err) => {
+			console.log(err);
+			pool.end();
+		});
+};
+
+const dropBlogPostTable = () => {
+	const queryText = 'DROP TABLE IF EXISTS blog_posts returning *';
+	pool.query(queryText)
+		.then((res) => {
+			console.log(res);
+			pool.end();
+		})
+		.catch((err) => {
+			console.log(err);
+			pool.end();
+		});
+};
+
+const createAllTables = () => {
+	createUserTable();
+	createBlogPostTable();
+};
+
+const dropAllTables = () => {
+	dropUserTable();
+	dropBlogPostTable();
+};
+
+pool.on('remove', () => {
+	console.log('client removed');
+	process.exit(0);
+});
+
+module.exports = {
+	createUserTable,
+	createBlogPostTable,
+	createAllTables,
+	dropUserTable,
+	dropBlogPostTable,
+	dropAllTables
+};
+
+require('make-runnable');
