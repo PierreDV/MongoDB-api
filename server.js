d3d40949890468ca6a0a643523cabbf9553157c5
PierreDV <p.verteuil@gmail.com>
2019-02-10 19:16:13 +0100

    add user controller, add method to create user record in database, hook up endpoint to server as post request

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import User from './src/controllers/User';
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.use(express.json);
 app.get('/', (req, res) => {
 	return res.status(200).send({'message': 'Your enpoint is working'});
 });
+
+app.post('/api/v1/users', User.create);
 
 app.listen(8080);
 console.log('app running on port 8080');
---
+++ src/controllers/User.js
@@ -0,0 +1,41 @@
+import moment from 'moment';
+import uuidv4 from 'uuid/v4';
+import db from '../db';
+import Helper from './Helper';
+
+const User = {
+	async create(req, res) {
+		if (!req.body.email || !req.body.password) {
+			return res.status(400).send({'message': 'Some values are missing.'});
+		}
+		if (!Helper.isValidEmail(req.body.email)) {
+			return res.status(400).send({'message': 'Please enter a valid email address.'});
+		}
+		const hashPassword = Helper.hashPassword(req.body.password);
+
+		const createQuery = `INSERT INTO
+			users(id, email, password, created_at, modified_at)
+			VALUES($1, $2, $3, $4, $5)
+			returning *`;
+		const values = [
+			uuidv4(),
+			req.body.email,
+			hashPassword,
+			moment(new Date()),
+			moment(new Date())
+		];
+
+		try {
+			const { rows } = await db.query(createQuery, values);
+			const token = Helper.generateToken(rows[0].id);
+			return res.status(201).send({ token });
+		} catch(error) {
+			if(error.routine === '_bt_check_unique') {
+				return res.status(400).send({ 'message': 'User with that email already exists.'});
+			}
+			return res.status(400).send(error);
+		}
+	}
+}
+
+export default User;
