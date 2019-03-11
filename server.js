5cebf83c504b47e2665937e852bca7fd9f8fe8e2
PierreDV <p.verteuil@gmail.com>
2019-03-11 21:50:01 +0100

    refactored cors code

--- server.js
+++ server.js
@@ -7,18 +7,7 @@ import Auth from './src/middleware/Auth';
 
 const app = express();
 
-const whitelist = ['http://localhost:3000'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
 // Default route
 app.get('/', (req, res) => {
